8027cf87674f5799285408def97cc0da6410d90e
Yağnur Tetikcan <50737107+yagnurl@users.noreply.github.com>
2021-09-19 00:46:04 +0300

    init

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,17 @@
+import "./topbar.scss";
+import "../../global.scss";
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="wrapper">
+                <div className="left">
+                <a href="#intro">LOGO</a>
+                </div>
+                <div className="right">
+
+                </div>
+            </div>
+        </div>
+    )
+}
