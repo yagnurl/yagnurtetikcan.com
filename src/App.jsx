6d7e05559db7e38aab7bd1e627deb99d4784970e
Yağnur Tetikcan <50737107+yagnurl@users.noreply.github.com>
2021-09-24 23:15:52 +0300

    menu has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import "./app.scss";
+import {useState} from 'react'
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Works from "./components/works/Works";
 import Portfolio from "./components/portfolio/Portfolio";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(true);
   return (
   <div className="app">
-    <Topbar />
+    <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+    <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
     <div className="sections">
       <Intro/>
       <Portfolio />
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,16 +1,34 @@
-import "./topbar.scss";
-import "../../global.scss";
+import "./topbar.scss"
 
-export default function Topbar() {
+import { Person, Mail} from '@mui/icons-material';
+
+
+
+export default function Topbar({menuOpen, setMenuOpen}) {
     return (
-        <div className="topbar">
+        <div className={`topbar ${(menuOpen && "active")}` }>
             <div className="wrapper">
                 <div className="left">
-                <a href="#intro">LOGO</a>
+                <a href="#intro" className='logo'>yagnur.</a>
+                <div className="item-container">
+                <Person className="icon" />
+                <span>+90 507 187 0034</span>
                 </div>
+                <div className="item-container">
+               <Mail className="icon"/>
+                <span>yagnurtetikcan@gmail.com</span>
+                </div>
+               </div>
+             
                 <div className="right">
-
+                  <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        <span className="line1"></span>
+        <span className="line2"></span>
+        <span className="line3"></span>
+        
+                  </div>
                 </div>
+            
             </div>
         </div>
     )
