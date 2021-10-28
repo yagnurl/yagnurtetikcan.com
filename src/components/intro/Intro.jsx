c9b8bc4b275e6e3dbd85946e28e249585a7d8eb0
Yağnur Tetikcan <50737107+yagnurl@users.noreply.github.com>
2021-10-28 22:16:50 +0300

    intro section is comletedn and ityped added

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,44 @@
 import "./intro.scss"
+import {init} from 'ityped'
+import {useEffect, useRef} from "react"
+
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+
+    useEffect(() => {
+       init(textRef.current, 
+        { 
+        showCursor: true, 
+        strings: ['Developer', 'Designer', 'Tester' ] ,
+        backDelay: 1500,
+        backSpeed: 60
+       
+    
+        })
+       
+    }, [])
+
+
     return (
         <div className="intro" id="intro">
-            INTRO
+            <div className="left">
+              <div className="imgContainer">
+                  <img src="assets/hero1.png" alt="" />
+              </div>
+            </div>
+            <div className="right">
+            <div className="wrapper">
+                <h2>Hi there, I'm</h2>
+                <h1>Yagnur Tetikcan</h1>
+                <h3>Freelance <span ref={textRef}></span> </h3>
+            </div>
+            <a href="#portfolio">
+                <img src="assets/down.png" alt="" />
+            </a>
+            </div>
         </div>
     )
 }
