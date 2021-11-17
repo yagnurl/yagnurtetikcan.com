9d46b2863b5bbd88f0bd971b18d2ddd676276703
Yağnur Tetikcan <50737107+yagnurl@users.noreply.github.com>
2021-11-17 23:16:52 +0300

    topbar style fixed

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -6,22 +6,19 @@ export default function Topbar({menuOpen, setMenuOpen}) {
         <div className={`topbar ${(menuOpen && "active")}` }>
             <div className="wrapper">
                 <div className="left">
-                <a href="#intro" className='logo'>yagnur.</a>
-                <div className="item-container">
-                <Person className="icon" />
-                <span>+90 507 187 0034</span>
+                  
                 </div>
-                <div className="item-container">
-               <Mail className="icon"/>
-                <span>yagnurtetikcan@gmail.com</span>
+                <div className="left">
+                    <a href="#intro" className='logo'>
+                    <img src="assets/yagnur.png" alt="" />
+                    </a>
                 </div>
-               </div>
              
                 <div className="right">
                   <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
-        <span className="line1"></span>
-        <span className="line2"></span>
-        <span className="line3"></span>
+                    <span className="line1"></span>
+                    <span className="line2"></span>
+                    <span className="line3"></span>
         
                   </div>
                 </div>
