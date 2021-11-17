a6f69445ad82b9fde56f567c59b29f85acea54a0
Yağnur Tetikcan <50737107+yagnurl@users.noreply.github.com>
2021-11-17 21:46:43 +0300

    contact added and responsive bugs fixed

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import {useState} from 'react'
 import "./contact.scss"
 
 export default function Contact() {
+    const [message, setMessage]  = useState(false);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessage(true);
+
+    }
     return (
         <div className="contact" id="contact">
-          <div className="left"></div>
-          <div className="right"></div>
+          <div className="left">
+              <img src="assets/shake.svg" alt="" />
+          </div>
+          <div className="right">
+              <h2>Contact.</h2>
+                  <form action="" onSubmit={handleSubmit}>
+                      <input type="text" placeholder="Email " />
+                      <textarea placeholder="Message" name="" id="" cols="30" rows="10"></textarea>
+                      <button type="submit">Send</button>
+                      {message && <span>Thanks, I will reply ASAP :) </span> }
+                  </form>
+            
+          </div>
         </div>
     )
 }
