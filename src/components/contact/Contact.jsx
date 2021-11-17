6f1297210708675ca72224c50fa54e258fb40c3a
Yağnur Tetikcan <50737107+yagnurl@users.noreply.github.com>
2021-11-17 20:22:41 +0300

    Testimonails section done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,7 +4,8 @@ import "./contact.scss"
 export default function Contact() {
     return (
         <div className="contact" id="contact">
-            CONTACT
+          <div className="left"></div>
+          <div className="right"></div>
         </div>
     )
 }
